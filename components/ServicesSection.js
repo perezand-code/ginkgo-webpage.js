867729906175299d4918087a2dd6e2dec9de9555
perezand-code <perezand@iu.edu>
2026-05-11 10:31:45 -0400

    Delete components/ServicesSection.js

--- components/ServicesSection.js
+++
@@ -1,4 +0,0 @@
-{services.map((service, index) => (
-            <div key={`${tab}-${service.title}`} className="service-card-enter opacity-0">
-            <div key={`${tab}-${service.title}`} className="service-card-enter">
-              <ServiceCard service={service} commercial={tab === "commercial"} index={index} />
